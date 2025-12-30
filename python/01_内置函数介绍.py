17b6cb5bac4e88539a7092bbf81e01b0d691b3fc
wb_mapengtao <wb_mapengtao@kuaishou.com>
2025-12-30 19:54:31 +0800

    python 内置函数

---
+++ python/01_内置函数介绍.py
@@ -0,0 +1,15 @@
+"""
+类的内置函数: 
+[
+    __new__, __init__, __del__, 
+    __call__, 
+    __hash__, __eq__, 
+    __lt__, __le__, __gt__, __ge__, __ne__,
+    __getattr__, __getattribute__, __setattr__, __delattr__,
+    __len__, __getitem__, __setitem__, __delitem__, __iter__, __next__,
+    __add__, __sub__, __mul__, __truediv__, 
+    __iadd__, __isub__,
+    __str__, __repr__
+]
+高优函数：
+"""
